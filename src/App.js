a04443f01275745610cb661fb2c6dc277097f416
Shivraj <124032075+ShivrajCodes@users.noreply.github.com>
2024-07-17 21:23:43 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, NavLink, Routes, Route, useNavigate } from 'react-router-dom';
-import Toggle from 'react-toggle';
-import 'react-toggle/style.css';
 import styled, { keyframes } from 'styled-components';
 import './App.css';
 import About from './components/About';
 import Blog from './components/Blog';
 import Contact from './components/Contact';
+import Home from './components/Home';  // Import Home component
 
 const fadeInUp = keyframes`
   0% {
@@ -20,23 +19,40 @@ const fadeInUp = keyframes`
 `;
 
 const AppContainer = styled.div`
-  background-image: url('/images/images.jpg');
+  background-image: url('/images/images.png');
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-  color: ${props => (props.isDarkMode ? '#ffffff' : '#000000')};
   min-height: 100vh;
   transition: all 0.3s;
   position: relative;
   animation: ${fadeInUp} 0.5s ease-in-out;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(255, 255, 255, 0.6); /* White overlay with 60% opacity */
+    z-index: 1;
+  }
+`;
+
+const ContentWrapper = styled.div`
+  position: relative;
+  z-index: 2; /* Ensure the content is above the overlay */
 `;
 
 const Navbar = styled.nav`
   display: flex;
   justify-content: center;
   padding: 20px;
-  border-bottom: ${props => (props.isDarkMode ? 'none' : '1px solid #eaeaea')};
-  background-color: ${props => (props.isDarkMode ? 'rgba(8, 10, 9, 0.8)' : 'rgba(255, 255, 255, 0.8)')};
+  border-bottom: 1px solid #eaeaea;
   animation: ${fadeInUp} 0.5s ease-in-out;
 
   @media (max-width: 768px) {
@@ -49,30 +65,16 @@ const Navbar = styled.nav`
 const Navlink = styled(NavLink)`
   margin: 0 20px;
   text-decoration: none;
-  color: ${props => (props.isDarkMode ? '#ffffff' : '#000000')};
+  color: black;
   font-size: 18px;
   font-weight: 500;
   position: relative;
   &.active {
-    text-decoration: underline;
-    text-decoration-color: red;
-    text-decoration-thickness: 3px;
+    color: red;
   }
 
   @media (max-width: 768px) {
     margin: 10px 0;
-  }
-`;
-
-const ToggleButton = styled.div`
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  cursor: pointer;
-
-  @media (max-width: 768px) {
-    top: 20px;
-    right: 20px;
   }
 `;
 
@@ -91,149 +93,19 @@ const MenuButton = styled.div`
 const MenuIcon = styled.div`
   width: 25px;
   height: 3px;
-  background-color: ${props => (props.isDarkMode ? '#ffffff' : '#000000')};
+  background-color: #000000;
   margin: 4px 0;
 `;
 
-const HomeContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: flex-start;
-  padding: 40px;
-  
-  background-color: ${props => (props.isDarkMode ? 'rgba(8, 10, 9, 0.8)' : 'rgba(255, 255, 255, 0.8)')};
-  color: ${props => (props.isDarkMode ? '#ffffff' : '#000000')};
-  transition: all 0.3s;
-
-  @media (max-width: 1024px) {
-    flex-direction: column;
-    padding: 20px;
-  }
-`;
-
-const TextContainer = styled.div`
-  flex: 1;
-  padding-right: 20px;
-
-  @media (max-width: 1024px) {
-    padding-right: 0;
-  }
-`;
-
-const ImageContainer = styled.div`
-  flex: 1;
-  height: 400px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding-top: 140px;
-
-  @media (max-width: 1024px) {
-    padding-top: 20px;
-  }
-
-  img {
-    max-width: 100%;
-    max-height: 100%;
-    background-color: transparent;
-  }
-`;
-
-const fadeIn = keyframes`
-  to {
-    opacity: 1;
-  }
-`;
-
-const Title = styled.h1`
-  font-size: 4em;
-  margin: 0 0 20px 0;
-  color: #FF66CC;
-  opacity: 0;
-  animation: ${fadeIn} 2s forwards;
-  animation-delay: 0s;
-
-  @media (max-width: 768px) {
-    font-size: 3em;
-  }
-`;
-
-const Subtitle = styled.h2`
-  font-size: 3em;
-  margin: 20px 0;
-  color: #CC33FF;
-  opacity: 0;
-  animation: ${fadeIn} 2s forwards;
-  animation-delay: 1s;
-
-  @media (max-width: 768px) {
-    font-size: 2em;
-  }
-`;
-
-const Description = styled.h3`
-  font-size: 2em;
-  margin: 20px 0;
-  color: #9933FF;
-  opacity: 0;
-  animation: ${fadeIn} 2s forwards;
-  animation-delay: 2s;
-
-  @media (max-width: 768px) {
-    font-size: 1.5em;
-  }
-`;
-
-const DetailedDescription = styled.p`
-  font-size: 1.2em;
-  line-height: 1.5;
-  opacity: 0;
-  animation: ${fadeIn} 2s forwards;
-  animation-delay: 3s;
-
-  @media (max-width: 768px) {
-    font-size: 1em;
-  }
-`;
-
-const Home = ({ isDarkMode }) => {
-  return (
-    <HomeContainer isDarkMode={isDarkMode}>
-      <TextContainer>
-        <Title>Welcome 👋🏻</Title>
-        <Subtitle>Presenting QCVSS: Quadra Care Vital Sync System</Subtitle>
-        <Description>By TEAM ARAGYA</Description>
-        <DetailedDescription>
-          About the QCVSS: QUADRA CARE VITAL SYNC SYSTEM
-          <br /><br />
-          Designed for effortless patient care, Quadra Care Vital Sync System is a portable ventilator merged with ECG, pulse rate monitoring, and saline level monitoring system together in a single setup. It provides accurate respiratory support and continuous cardiovascular monitoring. The ventilator uses advanced sensors and controls the algorithms, while on the other hand, the ECG attributes detection of real-life cardiac issues. The system is ideal for exigency as its compact design ensures easy transport and portability. Apart from that, an automated saline water monitoring system is also included to ensure the safe administration of saline monitoring solution to patients. Sensors are utilized in the system to measure the saline level in the bottle, which will timely alert whenever the bottle is going to be empty and need a replacement. Integrated data loggers and transmitters collect and send real-time data to monitoring stations or cloud-based platforms for analysis, allowing healthcare providers to make decisions. The system enhances patient care and safety in medical treatment by ensuring precise delivery and continuous monitoring.
-        </DetailedDescription>
-      </TextContainer>
-      <ImageContainer>
-        <img src="/images/QVCSS.png" alt="QVCSS" />
-      </ImageContainer>
-    </HomeContainer>
-  );
-};
-
 const PageContainer = styled.div`
-  background-color: ${props => (props.isDarkMode ? 'rgba(8, 10, 9, 0.8)' : 'rgba(255, 255, 255, 0.8)')};
-  color: ${props => (props.isDarkMode ? '#ffffff' : '#000000')};
-  
   transition: all 0.3s;
   animation: ${fadeInUp} 0.5s ease-in-out;
 `;
 
 const App = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
   const [isTransitioning, setIsTransitioning] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const navigate = useNavigate();
-
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-  };
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -248,39 +120,34 @@ const App = () => {
   };
 
   return (
-    <AppContainer isDarkMode={isDarkMode}>
-      <ToggleButton>
-        <Toggle
-          checked={isDarkMode}
-          onChange={toggleDarkMode}
-          aria-label="Dark mode toggle"
-        />
-      </ToggleButton>
-      <MenuButton onClick={toggleMenu}>
-        <MenuIcon isDarkMode={isDarkMode} />
-        <MenuIcon isDarkMode={isDarkMode} />
-        <MenuIcon isDarkMode={isDarkMode} />
-      </MenuButton>
-      <Navbar isDarkMode={isDarkMode} isMenuOpen={isMenuOpen}>
-        <Navlink to="/" isDarkMode={isDarkMode} onClick={(e) => handleNavigation(e, '/')}>
-          Home
-        </Navlink>
-        <Navlink to="/about" isDarkMode={isDarkMode} onClick={(e) => handleNavigation(e, '/about')}>
-          Instructions
-        </Navlink>
-        <Navlink to="/blog" isDarkMode={isDarkMode} onClick={(e) => handleNavigation(e, '/blog')}>
-          Components Used
-        </Navlink>
-        <Navlink to="/contact" isDarkMode={isDarkMode} onClick={(e) => handleNavigation(e, '/contact')}>
-          About Us
-        </Navlink>
-      </Navbar>
-      <Routes>
-        <Route path="/" element={<Home isDarkMode={isDarkMode} />} />
-        <Route path="/about" element={<PageContainer isDarkMode={isDarkMode}><About /></PageContainer>} />
-        <Route path="/blog" element={<PageContainer isDarkMode={isDarkMode}><Blog /></PageContainer>} />
-        <Route path="/contact" element={<PageContainer isDarkMode={isDarkMode}><Contact /></PageContainer>} />
-      </Routes>
+    <AppContainer>
+      <ContentWrapper>
+        <MenuButton onClick={toggleMenu}>
+          <MenuIcon />
+          <MenuIcon />
+          <MenuIcon />
+        </MenuButton>
+        <Navbar isMenuOpen={isMenuOpen}>
+          <Navlink to="/" onClick={(e) => handleNavigation(e, '/')}>
+            Home
+          </Navlink>
+          <Navlink to="/about" onClick={(e) => handleNavigation(e, '/about')}>
+            Instructions
+          </Navlink>
+          <Navlink to="/blog" onClick={(e) => handleNavigation(e, '/blog')}>
+            Components Used
+          </Navlink>
+          <Navlink to="/contact" onClick={(e) => handleNavigation(e, '/contact')}>
+            About Us
+          </Navlink>
+        </Navbar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<PageContainer><About /></PageContainer>} />
+          <Route path="/blog" element={<PageContainer><Blog /></PageContainer>} />
+          <Route path="/contact" element={<PageContainer><Contact /></PageContainer>} />
+        </Routes>
+      </ContentWrapper>
     </AppContainer>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,32 +7,133 @@ const fadeIn = keyframes`
   }
 `;
 
+const gradientAnimation = keyframes`
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+`;
+
 const HomeContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  text-align: center;
-  background-color: ${props => (props.isDarkMode ? '#333333' : '#ffffff')};
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 40px;
+
+  background-color: ${props => (props.isDarkMode ? 'rgba(8, 10, 9, 0.8)' : 'rgba(255, 255, 255, 0)')};
   color: ${props => (props.isDarkMode ? '#ffffff' : '#000000')};
   transition: all 0.3s;
+
+  @media (max-width: 1024px) {
+    padding: 20px;
+  }
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  width: 100%;
+
+  @media (max-width: 1024px) {
+    flex-direction: column;
+  }
+`;
+
+const TextContainer = styled.div`
+  flex: 1;
+  padding-right: 20px;
+  opacity: 0;
+  animation: ${fadeIn} 2s forwards;
+
+  @media (max-width: 1024px) {
+    padding-right: 0;
+  }
+`;
+
+const ImageContainer = styled.div`
+  flex: 1;
+  height: 400px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-top: 140px;
+
+  @media (max-width: 1024px) {
+    padding-top: 20px;
+  }
+
+  img {
+    max-width: 100%;
+    max-height: 100%;
+    background-color: transparent;
+  }
 `;
 
 const Title = styled.h1`
   font-size: 3em;
   margin: 0 0 20px 0;
+  color: #FF66CC;
   opacity: 0;
   animation: ${fadeIn} 2s forwards;
+
+  @media (max-width: 768px) {
+    font-size: 2.5em;
+  }
 `;
 
-const Description = styled.p`
-  font-size: 1.2em;
+const Subtitle = styled.h2`
+  font-size: 2.5em;
   margin: 20px 0;
+  background: linear-gradient(270deg, #FF66CC, #CC33FF, #9933FF);
+  background-size: 600% 600%;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  animation: ${fadeIn} 2s forwards, ${gradientAnimation} 8s ease infinite;
+  animation-delay: 1s;
+
+  @media (max-width: 768px) {
+    font-size: 2em;
+  }
+`;
+
+const Description = styled.h3`
+  font-size: 2em;
+  margin: 20px 0;
+  color: #9933FF;
+  opacity: 0;
+  animation: ${fadeIn} 2s forwards;
+  animation-delay: 2s;
+
+  @media (max-width: 768px) {
+    font-size: 1.5em;
+  }
+`;
+
+const DetailedDescription = styled.p`
+  font-size: 1.2em;
+  line-height: 1.5;
+  opacity: 0;
+  animation: ${fadeIn} 2s forwards;
+  animation-delay: 3s;
+
+  @media (max-width: 768px) {
+    font-size: 1em;
+  }
 `;
 
 const Links = styled.div`
   margin-top: 20px;
+  opacity: 0;
+  animation: ${fadeIn} 2s forwards;
+  animation-delay: 3.5s;
 `;
 
 const Link = styled.a`
@@ -43,18 +144,239 @@ const Link = styled.a`
   transition: color 0.3s;
 `;
 
+const WhyChooseSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 40px;
+  opacity: 0;
+  animation: ${fadeIn} 2s forwards;
+  animation-delay: 4s;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const DoctorImage = styled.img`
+  flex: 1;
+  max-width: 100%;
+  height: auto;
+
+  @media (max-width: 768px) {
+    order: 2;
+    margin-top: 20px;
+  }
+`;
+
+const WhyChooseText = styled.div`
+  flex: 2;
+  padding-left: 20px;
+
+  @media (max-width: 768px) {
+    padding-left: 0;
+  }
+`;
+
+const WhyChooseTitle = styled.h2`
+  font-size: 2.5em;
+  color: #9933FF;
+  margin: 0 0 20px 0;
+`;
+
+const WhyChooseDescription = styled.p`
+  font-size: 1.2em;
+  line-height: 1.5;
+`;
+
+const ReviewSection = styled.div`
+  margin-top: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const Card = styled.div`
+  flex: 1;
+  background: rgba(255, 255, 255, 0.1);
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  margin: 0 10px;
+
+  @media (max-width: 768px) {
+    margin: 10px 0;
+  }
+`;
+
+const UserImage = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-right: 10px;
+`;
+
+const UserName = styled.h4`
+  margin: 0;
+  font-size: 1.2em;
+`;
+
+const ReviewText = styled.p`
+  font-size: 1em;
+  margin: 10px 0;
+`;
+
+const StarRating = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Star = styled.span`
+  color: #FFD700;
+  margin-right: 5px;
+`;
+
+const FreeTrialContainer = styled.div`
+  text-align: center;
+  margin-top: 40px;
+  opacity: 0;
+  animation: ${fadeIn} 2s forwards;
+  animation-delay: 4.5s;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const FreeTrialText = styled.p`
+  font-size: 1.5em;
+  color: #9933FF;
+  margin: 0;
+  @media (max-width: 768px) {
+    font-size: 1.2em;
+  }
+`;
+
+const FreeTrialButton = styled.button`
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 1.2em;
+  color: #fff;
+  background-color: #9933FF;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  position: relative;
+
+  @media (max-width: 768px) {
+    font-size: 1em;
+  }
+
+  &:hover .popup {
+    display: block;
+  }
+`;
+
+const Popup = styled.div`
+  display: none;
+  position: absolute;
+  bottom: 100%;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: #333;
+  color: #fff;
+  padding: 5px 10px;
+  border-radius: 3px;
+  font-size: 0.8em;
+`;
+
+const UserReviewHeader = styled.h2`
+  font-size: 2.5em;
+  color: #FF66CC;
+  margin-top: 40px;
+  opacity: 0;
+  animation: ${fadeIn} 2s forwards;
+`;
+
 const Home = ({ isDarkMode }) => {
   return (
     <HomeContainer isDarkMode={isDarkMode}>
-      <Title>Hello <span role="img" aria-label="wave">👋</span></Title>
-      <Description>I'm Chetan Verma - curious designer and developer based in New Delhi, India.</Description>
-      <Links>
-        <Link href="https://github.com" isDarkMode={isDarkMode}>Github</Link>
-        <Link href="https://linkedin.com" isDarkMode={isDarkMode}>LinkedIn</Link>
-        <Link href="https://twitter.com" isDarkMode={isDarkMode}>Twitter</Link>
-        <Link href="https://blog.com" isDarkMode={isDarkMode}>Blog</Link>
-        <Link href="mailto:chetan@example.com" isDarkMode={isDarkMode}>Email</Link>
-      </Links>
+      <ContentContainer>
+        <TextContainer>
+          <Title>Welcome 👋🏻</Title>
+          <Subtitle>Presenting QCVSS: Quadra Care Vital Sync System</Subtitle>
+          <Description>By TEAM ARAGYA</Description>
+          <DetailedDescription>
+            About the QCVSS: QUADRA CARE VITAL SYNC SYSTEM
+            <br /><br />
+            Designed for effortless patient care, Quadra Care Vital Sync System is a portable ventilator merged with ECG, pulse rate monitoring, and saline level monitoring system together in a single setup. It provides accurate respiratory support and continuous cardiovascular monitoring. The ventilator uses advanced sensors and controls the algorithms, while on the other hand, the ECG attributes detection of real-time cardiac issues, which is ideal for exigency as its compact design ensures easy transport and portability. The system includes an automated saline water monitoring system, utilizing sensors to measure the saline level in the bottle and alert when it needs replacement. Integrated data loggers and transmitters collect and send real-time data to monitoring stations or cloud-based platforms for analysis, allowing healthcare providers to make decisions. The system enhances patient care and safety by ensuring precise delivery and continuous monitoring.
+          </DetailedDescription>
+        </TextContainer>
+        <ImageContainer>
+          <img src="/images/QVCSS.png" alt="QVCSS" />
+        </ImageContainer>
+      </ContentContainer>
+
+      <WhyChooseSection>
+        <DoctorImage src="/images/doctor-placeholder.jpg" alt="Doctor" />
+        <WhyChooseText>
+          <WhyChooseTitle>Why Choose QCVSS</WhyChooseTitle>
+          <WhyChooseDescription>
+            QCVSS integrates multiple vital monitoring systems into one portable device, making it a convenient and efficient choice for healthcare providers. The advanced technology and compact design ensure accurate and continuous monitoring, enhancing patient care and safety.
+          </WhyChooseDescription>
+        </WhyChooseText>
+      </WhyChooseSection>
+
+      <UserReviewHeader>WHAT OUR USERS HAVE TO SAY</UserReviewHeader>
+
+      <ReviewSection>
+        <Card>
+          <StarRating>
+            <UserImage src="/images/user-placeholder.jpg" alt="User" />
+            <UserName>John Doe</UserName>
+          </StarRating>
+          <ReviewText>QCVSS has transformed our patient care approach. The integrated monitoring system is a game-changer!</ReviewText>
+          <StarRating>
+            {[...Array(4)].map((star, index) => (
+              <Star key={index}>★</Star>
+            ))}
+          </StarRating>
+        </Card>
+        <Card>
+          <StarRating>
+            <UserImage src="/images/user-placeholder.jpg" alt="User" />
+            <UserName>Jane Smith</UserName>
+          </StarRating>
+          <ReviewText>The portability and accuracy of QCVSS have significantly improved our response time in emergencies.</ReviewText>
+          <StarRating>
+            {[...Array(4)].map((star, index) => (
+              <Star key={index}>★</Star>
+            ))}
+          </StarRating>
+        </Card>
+        <Card>
+          <StarRating>
+            <UserImage src="/images/user-placeholder.jpg" alt="User" />
+            <UserName>Dr. Emily Clark</UserName>
+          </StarRating>
+          <ReviewText>An essential tool for modern healthcare. QCVSS delivers reliable and comprehensive patient monitoring.</ReviewText>
+          <StarRating>
+            {[...Array(4)].map((star, index) => (
+              <Star key={index}>★</Star>
+            ))}
+          </StarRating>
+        </Card>
+      </ReviewSection>
+
+      <FreeTrialContainer>
+        <FreeTrialText>What are you waiting for, get a free trial today!</FreeTrialText>
+        <FreeTrialButton>
+          Get A Free Trial Today
+          <Popup className="popup">Coming soon</Popup>
+        </FreeTrialButton>
+      </FreeTrialContainer>
     </HomeContainer>
   );
 };
